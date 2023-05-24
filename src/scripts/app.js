ccf1555a997fb1693ca0f7d11d01a23db349d839
Aldriiss <120035959+Aldriiss@users.noreply.github.com>
2023-05-24 06:03:52 -0700

    Modifications Ouverture/fermeture nav (mobile)

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,10 +3,24 @@
 let burgerButton = document.querySelector(".burger");
 let menu = document.querySelector(".nav__menu");
 let body = document.querySelector("body");
+let navEl = document.querySelectorAll(".nav__lien");
+
 
 if(burgerButton){
     burgerButton.addEventListener("click", () => {
         burgerButton.classList.toggle('active');
         menu.classList.toggle('switch');
-        body.classList.toggle('overflow')
-    })};
+        body.classList.toggle('overflow');
+        
+
+    })
+    for (const el of navEl) {
+    el.addEventListener("click", Switch )
+    }
+    function Switch(){
+        menu.classList.remove("switch");
+        burgerButton.classList.remove('active');
+    }
+
+};
+
