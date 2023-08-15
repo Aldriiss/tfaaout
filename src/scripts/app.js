a6e3510aedf399ec60d0b10df203fc540b015ede
Aldriiss <120035959+Aldriiss@users.noreply.github.com>
2023-08-15 14:51:00 +0200

    Design + contenue + refonte totale

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,80 +1,323 @@
-"use strict";
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
 
-let burgerButton = document.querySelector(".burger");
-let menu = document.querySelector(".nav__menu");
-let body = document.querySelector("body");
-let navEl = document.querySelectorAll(".nav__lien");
-
-if (burgerButton) {
-  burgerButton.addEventListener("click", () => {
-    burgerButton.classList.toggle('active');
-    menu.classList.toggle('switch');
+/***/ "./src/scripts/app.js":
+/*!****************************!*\
+  !*** ./src/scripts/app.js ***!
+  \****************************/
+/***/ (() => {
 
 
-  })
-  for (const el of navEl) {
-    el.addEventListener("click", Switch)
+
+        function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() { }; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+        function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+        function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+        var burgerButton = document.querySelector(".burger");
+        var menu = document.querySelector(".nav__menu");
+        var body = document.querySelector("body");
+        var navEl = document.querySelectorAll(".nav__lien");
+
+        if (burgerButton) {
+          var Switch = function Switch() {
+            menu.classList.remove("switch");
+            burgerButton.classList.remove('active');
+          };
+
+          burgerButton.addEventListener("click", function () {
+            burgerButton.classList.toggle('active');
+            menu.classList.toggle('switch');
+          });
+
+          var _iterator = _createForOfIteratorHelper(navEl),
+            _step;
+
+          try {
+            for (_iterator.s(); !(_step = _iterator.n()).done;) {
+              var el = _step.value;
+              el.addEventListener("click", Switch);
+            }
+          } catch (err) {
+            _iterator.e(err);
+          } finally {
+            _iterator.f();
+          }
+        }
+
+        ;
+        var menuItems = document.querySelectorAll('.nav__el');
+
+        function animateMenuItems() {
+          menuItems.forEach(function (item, index) {
+            setTimeout(function () {
+              item.classList.add('slide-in');
+            }, index * 100); // Ajoute un délai d'apparition progressif
+          });
+        }
+
+        var googlegrap = document.querySelector('.dia');
+
+        if (googlegrap) {
+          // premier Graphique
+          var drawChart1 = function drawChart1() {
+            // Donnée du graph1
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Topping');
+            data.addColumn('number', 'Slices');
+            data.addRows([['Oui Beaucoup', 2], ['Oui', 6], ['Moyennement', 5], ['Non', 3]]);
+            var chart = new google.visualization.PieChart(document.getElementById('chart_div--1'));
+            chart.draw(data);
+          }; // Second Graphique
+
+
+          var drawChart2 = function drawChart2() {
+            // Donnée du graph2
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Topping');
+            data.addColumn('number', 'Slices');
+            data.addRows([['Le temps', 7], ['Le prix', 3], ['La vaisselles', 2], ['Le manque d\'\idées', 5]]);
+            var chart = new google.visualization.PieChart(document.getElementById('chart_div--2'));
+            chart.draw(data);
+          };
+
+          // API pour les graph style google
+          google.charts.load('current', {
+            'packages': ['corechart']
+          });
+          google.charts.setOnLoadCallback(function () {
+            drawChart1();
+            drawChart2();
+          });
+        }
+
+        /***/
+      }),
+
+/***/ "./src/styles/app.scss":
+/*!*****************************!*\
+  !*** ./src/styles/app.scss ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+        __webpack_require__.r(__webpack_exports__);
+        // extracted by mini-css-extract-plugin
+
+
+        /***/
+      })
+
+    /******/
+  });
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+      /******/
+    }
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+      /******/
+    };
+/******/
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+    /******/
   }
-  function Switch() {
-    menu.classList.remove("switch");
-    burgerButton.classList.remove('active');
-  }
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	(() => {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = (result, chunkIds, fn, priority) => {
+/******/ 			if (chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for (var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+        /******/
+      }
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var [chunkIds, fn, priority] = deferred[i];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+/******/ 						chunkIds.splice(j--, 1);
+            /******/
+          } else {
+/******/ 						fulfilled = false;
+/******/ 						if (priority < notFulfilled) notFulfilled = priority;
+            /******/
+          }
+          /******/
+        }
+/******/ 				if (fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+          /******/
+        }
+        /******/
+      }
+/******/ 			return result;
+      /******/
+    };
+    /******/
+  })();
+/******/
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+    /******/
+  })();
+/******/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+        /******/
+      }
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+      /******/
+    };
+    /******/
+  })();
+/******/
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"/scripts/app": 0,
+/******/ 			"styles/app": 0
+      /******/
+    };
+/******/
+/******/ 		// no chunk on demand loading
+/******/
+/******/ 		// no prefetching
+/******/
+/******/ 		// no preloaded
+/******/
+/******/ 		// no HMR
+/******/
+/******/ 		// no HMR manifest
+/******/
+/******/ 		__webpack_require__.O.j = (chunkId) => (installedChunks[chunkId] === 0);
+/******/
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			if (chunkIds.some((id) => (installedChunks[id] !== 0))) {
+/******/ 				for (moduleId in moreModules) {
+/******/ 					if (__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+            /******/
+          }
+          /******/
+        }
+/******/ 				if (runtime) var result = runtime(__webpack_require__);
+        /******/
+      }
+/******/ 			if (parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for (; i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if (__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+          /******/
+        }
+/******/ 				installedChunks[chunkId] = 0;
+        /******/
+      }
+/******/ 			return __webpack_require__.O(result);
+      /******/
+    }
+/******/
+/******/ 		var chunkLoadingGlobal = self["webpackChunk_dwmaj_workflow"] = self["webpackChunk_dwmaj_workflow"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+    /******/
+  })();
+/******/
+/************************************************************************/
+/******/
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	__webpack_require__.O(undefined, ["styles/app"], () => (__webpack_require__("./src/scripts/app.js")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["styles/app"], () => (__webpack_require__("./src/styles/app.scss")))
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+  /******/
+  /******/
+})()
+  ;
+//# sourceMappingURL=app.js.map
 
+
+
+let section = document.querySelectorAll("section");
+let menu = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  section.forEach((i) => {
+    let top = window.scrollY;
+    let offset = i.offsetTop - 150;
+    let height = i.offsetHeight;
+    let id = i.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      menu.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
 };
 
-let menuItems = document.querySelectorAll('.nav__el');
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
 
-function animateMenuItems() {
-  menuItems.forEach(function (item, index) {
-    setTimeout(function () {
-      item.classList.add('slide-in');
-    }, index * 100); // Ajoute un délai d'apparition progressif
-  });
-}
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
 
-let googlegrap = document.querySelector('.dia');
-
-if (googlegrap) {
-
-  // API pour les graph style google
-  google.charts.load('current', { 'packages': ['corechart'] });
-
-  google.charts.setOnLoadCallback(function () {
-    drawChart1();
-    drawChart2();
-  });
-
-  // premier Graphique
-  function drawChart1() {
-    // Donnée du graph1
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Topping');
-    data.addColumn('number', 'Slices');
-    data.addRows([
-      ['Oui Beaucoup', 2],
-      ['Oui', 6],
-      ['Moyennement', 5],
-      ['Non', 3],
-    ]);
-
-    var chart = new google.visualization.PieChart(document.getElementById('chart_div--1'));
-    chart.draw(data);
-  }
-
-  // Second Graphique
-  function drawChart2() {
-    // Donnée du graph2
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Topping');
-    data.addColumn('number', 'Slices');
-    data.addRows([
-      ['Le temps', 7],
-      ['Le prix', 3],
-      ['La vaisselles', 2],
-      ['Le manque d\'\idées', 5],
-    ]);
-
-    var chart = new google.visualization.PieChart(document.getElementById('chart_div--2'));
-    chart.draw(data);
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
   }
 }
+
+window.addEventListener("scroll", reveal);
+reveal();
+
